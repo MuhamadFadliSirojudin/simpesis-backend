a0bf9ba54fb5badbf4ea93ff09a90dd43a726075
Muhamad Fadli Sirojudin <mfadlisirojudin@gmail.com>
2025-06-13 10:17:54 +0700

    edit login aplikasi

--- controller/auth.js
+++ controller/auth.js
@@ -54,6 +54,8 @@ export const login = async (req, res) => {
 
   res.status(200).json({
     id: user.id,
+    username: user.username,
+    role: user.role, // tambahkan role
     accessToken: token
   })
 };
