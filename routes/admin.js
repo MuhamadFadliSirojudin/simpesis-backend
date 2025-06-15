6e8f016980cd28e1dc4ccc33a88bd0c1e9837189
Muhamad Fadli Sirojudin <mfadlisirojudin@gmail.com>
2025-06-16 03:15:59 +0700

    add akun admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,19 @@
 // routes/admin.js
 import express from "express";
-import { loginAdmin } from "../controller/admin.js";
-import { getAllAdmin, createAdmin } from "../controller/admin.js";
+import {
+  loginAdmin,
+  getAllAdmin,
+  createAdmin,
+  deleteAdminById,
+  updateAdmin
+} from "../controller/admin.js";
+
 const router = express.Router();
 
 router.post("/login", loginAdmin);
 router.get("/", getAllAdmin);
 router.post("/", createAdmin);
+router.delete("/:id", deleteAdminById);
+router.put("/:id", updateAdmin);
 
 export default router;
