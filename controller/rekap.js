e22c93ec7cc2710387f5635324b162c6bab65660
Muhamad Fadli Sirojudin <mfadlisirojudin@gmail.com>
2025-08-01 15:37:16 +0700

    add harian

--- controller/rekap.js
+++ controller/rekap.js
@@ -4,23 +4,13 @@ import prisma from "../lib/db.js";
 export const getRekapHarian = async (req, res) => {
   try {
     const nilai = await prisma.nilai.findMany({
-      where: {
-        modul: {
-          isNot: null,
-        },
-      },
       select: {
         id: true,
         id_siswa: true,
         nilai: true,
         createdAt: true,
         id_modul: true,
-        modul: {
-          select: {
-            topik: true,
-            nama: true,
-          },
-        },
+        modul: true,
       },
     });
 
