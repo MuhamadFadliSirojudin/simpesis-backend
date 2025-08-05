a9f40873b90c3b30d7096e614e1e64f424cf2f7b
Muhamad Fadli Sirojudin <mfadlisirojudin@gmail.com>
2025-08-05 12:31:38 +0700

    add harian

--- controller/rekap.js
+++ controller/rekap.js
@@ -10,21 +10,16 @@ export const getRekapHarian = async (req, res) => {
         nilai: true,
         createdAt: true,
       },
-      include: {
-        modul: true,
-      }
     });
 
     const grouped = nilai.reduce((acc, curr) => {
       const tanggal = curr.createdAt.toISOString().split("T")[0]; // YYYY-MM-DD
-      const modulId = item.id_modul;
-      const key = `${curr.id_siswa}-${tanggal}-${modulId}`;
+      const key = `${curr.id_siswa}`;
 
       if (!acc[key]) {
         acc[key] = {
           id_siswa: curr.id_siswa,
           tanggal,
-          modul: item.modul?.topik || item.modul?.nama || "Tidak diketahui",
           jumlah: 0,
           total: 0,
         };
@@ -48,7 +43,6 @@ export const getRekapHarian = async (req, res) => {
       return {
         id_siswa: g.id_siswa,
         nama_siswa: siswa?.nama || "Tidak diketahui",
-        modul: item.modul,
         tanggal: g.tanggal,
         jumlah_nilai: g.total,
         rata_rata: parseFloat((g.total / g.jumlah).toFixed(1)),
